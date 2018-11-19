b3d3075e9cc38533a6e3c29e6c078e0b3d9777c4
Infratop <Infratop@infratopnoMacBook-Air.local>
2018-11-19 11:25:33 +0900

    [Finish]4-3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,6 +6,17 @@ class BlogsController < ApplicationController
   end
 
   def new
+  	@blog = Blog.new
+  end
+  def create
+  	blog = Blog.new(blog_params)
+  	blog.save
+  	redirect_to blogs_path
+  end
+
+  private
+  def blogs_params
+  	params.require(:blog).permit(:title, :category, :body)
   end
 
   def edit
